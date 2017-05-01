b83851747f1e8c6076b555318ca0da74a1fa5794
aryana373 <aryanadwiputra@gmail.com>
2017-05-02 02:03:04 +0800

    xxx

--- penyelenggara/index.php
+++ penyelenggara/index.php
@@ -6,7 +6,13 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    
+
+<style>
+  h3{
+    color: white;
+  }
+
+</style> 
     
     
   </head>
@@ -15,87 +21,219 @@
 
   <section id="container" >
       
-      
+      <!--sidebar start-->
+      <aside>
+          <div  id="sidebar"  class="nav-collapse ">
+              <!-- sidebar menu start-->
+              <ul class="sidebar-menu" id="nav-accordion">
+              
+                  <p class="centered"><a href="profile.html"><img src="assets/img/kim.png" class="img-circle" width="60"></a></p>
+                  <h5 class="centered"><?php echo $_SESSION['nama_lomba']; ?></h5> 
+                    
+                  <li class="mt">
+                      <a class="active"  href="index.php">
+                          <i class="fa fa-dashboard"></i>
+                          <span>Dashboard</span>
+                      </a>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="javascript:;" >
+                          <i class="fa fa-desktop"></i>
+                          <span>Data Tim</span>
+                      </a>
+                      <ul class="sub">
+                          <li><a  href="tim-belum-dikonfirmasi.php">Data Tim Terdaftar</a></li>
+                          <li><a  href="buttons.html">Data Tim Terkonfirmasi</a></li>
+                          
+                      </ul>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="javascript:;" >
+                          <i class="fa fa-cogs"></i>
+                          <span>Components</span>
+                      </a>
+                      <ul class="sub">
+                          <li><a  href="calendar.html">Calendar</a></li>
+                          <li><a  href="gallery.html">Gallery</a></li>
+                          <li><a  href="todo_list.html">Todo List</a></li>
+                      </ul>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="javascript:;" >
+                          <i class="fa fa-book"></i>
+                          <span>Extra Pages</span>
+                      </a>
+                      <ul class="sub">
+                          <li><a  href="blank.html">Blank Page</a></li>
+                          <li><a  href="login.html">Login</a></li>
+                          <li><a  href="lock_screen.html">Lock Screen</a></li>
+                      </ul>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="javascript:;" >
+                          <i class="fa fa-tasks"></i>
+                          <span>Forms</span>
+                      </a>
+                      <ul class="sub">
+                          <li><a  href="form_component.html">Form Components</a></li>
+                      </ul>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="javascript:;" >
+                          <i class="fa fa-th"></i>
+                          <span>Data Tables</span>
+                      </a>
+                      <ul class="sub">
+                          <li><a  href="basic_table.html">Basic Table</a></li>
+                          <li><a  href="responsive_table.html">Responsive Table</a></li>
+                      </ul>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="javascript:;" >
+                          <i class=" fa fa-bar-chart-o"></i>
+                          <span>Charts</span>
+                      </a>
+                      <ul class="sub">
+                          <li><a  href="morris.html">Morris</a></li>
+                          <li><a  href="chartjs.html">Chartjs</a></li>
+                      </ul>
+                  </li>
+
+              </ul>
+              <!-- sidebar menu end-->
+          </div>
+      </aside>
       <!-- **********************************************************************************************************************************************************
       MAIN CONTENT
       *********************************************************************************************************************************************************** -->
       <!--main content start-->
       <section id="main-content">
           <section class="wrapper">
-
-              <div class="row">
-                  <div class="col-lg-11 main-chart">
-                  
+            <div class="panel-heading">
+              <h1 style="text-transform: uppercase;color: black;text-align: center;"> SISTEM MAGAGEMENT <?php echo $_SESSION['nama_lomba']; ?></h1>
+            </div>
+            <div  class="col-md-4 col-sm-4 mb">
+                          <div style="background: red" class="darkblue-panel pn">
+                            <div class="darkblue-header">
+                    <h3>PESAN</h3>
+                            </div>
+                            <h1 class="mt"><i class="fa fa-comment fa-3x"></i></h1>
+                
+                <footer>
+                  <div class="centered">
+                    <h5><i class="fa fa-trophy"></i> 17,988</h5>
+                  </div>
+                </footer>
+                          </div><! -- /darkblue panel -->
+                        </div><!-- /col-md-4 -->
                     
-          <div id="page-wrapper"><br />
-      <h2 style="color: black"><center> Data Tim Peserta : <?php echo $_SESSION['nama_lomba']; ?> </center></h2><br />
 
-      <table style="color: black" class="table table-bordered table-striped">
-        <thead style="text-align: center; background-color:  #333333; color: white">
-            <td>No</td>
-            <td>Nama Tim</td>
-            <td>Alamat Tim</td>
-            <td>Penanggung jawab</td>
-            <td>Email</td>
-            <td>No HP</td>
-            <td>Pilihan</td>
-        </thead>
-        <tbody>
-      <?php
-      $id_penyelenggara = $_SESSION['id_penyelenggara'];
-          $sql = mysqli_query($koneksi, "SELECT * FROM tim where id_penyelenggara='$id_penyelenggara' ");
+                    <div class="col-md-4 col-sm-4 mb">
+                          <div style="background: #ffd11a" class="darkblue-panel pn">
+                            <div class="darkblue-header">
+                    <h3>TIM TERDAFTAR</h3>
+                            </div>
+                            <h1 class="mt"><i class="fa fa-user fa-3x"></i></h1>
+               
+                <footer>
+                  <div class="centered">
+                    <h5><i class="fa fa-trophy"></i> 17,988</h5>
+                  </div>
+                </footer>
+                          </div><! -- /darkblue panel -->
+                        </div><!-- /col-md-4 -->
+                   
 
-          if(mysqli_num_rows($sql) == 0){
-            echo '<tr style="text-align:center;"><td colspan="7">Empty</td></tr>';
-          }
-          else{
-            $i=1;
-          while($row = mysqli_fetch_assoc($sql)){
-            echo '
-            <tr>
-              
-              <td style="text-align: center">'.$i.'</td>
-              <td style="text-align: center">'.$row['nama_tim'].'</td>
-              <td style="text-align: center">'.$row['alamat_tim'].'</td>
-              <td style="text-align: center">'.$row['penanggung_jawab'].'</td>
-              <td style="text-align: center">'.$row['email_tim'].'</td>
-              <td style="text-align: center">'.$row['tlp_tim'].'</td>
-              <td style="text-align: center">
-                <a href="#" id=' .$row["id_penyelenggara"].' class="btn btn-sm btn-info"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span></a>
-                <a href="index.php?aksi=delete&id_tim='.$row['id_tim'].'" title="Hapus Data" onclick="return confirm(\'Anda yakin akan menghapus data '.$row['nama_tim'].'?\')" class="btn btn-danger btn-sm"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>
-              </td>
-            </tr>
-            ';
-            $i++;
-          }
-        }
-        ?>
-      </tbody>
-  </table>
 
-      </div><!-- /#page-wrapper -->
-          
-          
-          </div><!-- /col-lg-9 END SECTION MIDDLE -->
-                  
-                  
-      <!-- **********************************************************************************************************************************************************
-      RIGHT SIDEBAR CONTENT
-      *********************************************************************************************************************************************************** -->                  
-                  
+                    <div  class="col-md-4 col-sm-4 mb">
+                <div  class="green-panel pn">
+                  <div style="background: #68dff0"  class="green-header">
+                    <h3 >SLOT KOSONG</h3>
+                  </div>
+                <canvas id="serverstatus03" height="120" width="120"></canvas>
+                <script>
+                  var doughnutData = [
+                      {
+                        value: 60,
+                        color:"#2b2b2b"
+                      },
+                      {
+                        value : 40,
+                        color : "#fffffd"
+                      }
+                    ];
+                    var myDoughnut = new Chart(document.getElementById("serverstatus03").getContext("2d")).Doughnut(doughnutData);
+                </script>
+                <h5>SISA 3 SLOT</h5>
+                </div>
+              </div><! --/col-md-4 -->               
             
 
-                       
-                      
-                  </div><!-- /col-lg-3 -->
-              </div><! --/row -->
-          </section>
-      </section>
+              <div class="panel panel-info">
+           <h1 style="text-transform: uppercase;color: white;text-align: center;">  XXXXXXXXX</h1>
+            <div class="panel-body">
+              <div class="row">
+                <div class="col-md-4 col-lg-4 " align="center"> <img alt="User Pic" src="kim.png" class=" img-responsive"> </div>
+                
+                <!--<div class="col-xs-10 col-sm-10 hidden-md hidden-lg"> <br>
+                  <dl>
+                    <dt>DEPARTMENT:</dt>
+                    <dd>Administrator</dd>
+                    <dt>HIRE DATE</dt>
+                    <dd>11/12/2013</dd>
+                    <dt>DATE OF BIRTH</dt>
+                       <dd>11/12/2013</dd>
+                    <dt>GENDER</dt>
+                    <dd>Male</dd>
+                  </dl>
+                </div>-->
+                <div class=" col-md-8 col-lg-8 "> 
+                  <table class="table table-user-information">
+                    <tbody>
+                      <tr>
+                        <td><b>Nama Lomba</b></td>
+                        <td>Programming</td>
+                      </tr>
+                      <tr>
+                        <td><b>Penyelenggara</b></td>
+                        <td>06/23/2013</td>
+                      </tr>
+                      <tr>
+                        <td><b>Lokasi</b> </td>
+                        <td>01/24/1988</td>
+                      </tr>
+                         <td><b>Waktu Mulai</b> </td>
+                        <td>01/24/1988</td>
+                      </tr>
+                           <td><b>Waktu Selesai</b> </td>
+                        <td>01/24/1988</td>
+                      </tr>
+                        <td><b>Kategori</b> </td>
+                        <td>01/24/1988</td>
+                      </tr>
+                         <td><b>Email</b> </td>
+                        <td>01/24/1988</td>
+                      </tr>
+                         <td><b>No HP</b> </td>
+                        <td>01/24/1988</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  
+                  <a href="#" class="btn btn-primary">edit profil</a>
 
-      <!--main content end-->
+                 
+                </div>
+              </div>
+            </div>
+                 
+            
+          </div>
                   
-                  
-      
+          </section>
+      </section>           
       
   </section>
 
