e404b0d58b83f66d9f6a0ec11adf7a69ec58085b
aryana373 <aryanadwiputra@gmail.com>
2017-05-03 01:51:08 +0800

    kim

--- penyelenggara/proses-tambah-penyelenggara.php
+++ penyelenggara/proses-tambah-penyelenggara.php
@@ -23,6 +23,7 @@ include "../koneksi.php";
 					$path = "pembayaran_penyelenggara/".$fotobaru;
 					$id_level						=$_POST['id_level'];
 					$status_penyelenggara			=$_POST['status_penyelenggara'];
+					$logo							='logo.png';
 					
 					
 
@@ -31,7 +32,7 @@ include "../koneksi.php";
 					$cek = mysqli_query($koneksi, "SELECT * FROM penyelenggara WHERE username_penyelenggara='$username_penyelenggara'")or die (mysqli_error($koneksi));
 					if(mysqli_num_rows($cek) == 0)
 					{
-						$insert = mysqli_query($koneksi, "INSERT INTO penyelenggara(nama_penyelenggara, nama_lomba, lokasi_lomba, waktu_awal_lomba,waktu_akhir_lomba,id_kategori, email_penyelenggara, tlp_penyelenggara, username_penyelenggara, password_penyelenggara, pembayaran_penyelenggara, id_level, status_penyelenggara) VALUES('$nama_penyelenggara', '$nama_lomba', '$lokasi_lomba', '$waktu_awal_lomba', '$waktu_akhir_lomba', '$id_kategori', '$email_penyelenggara', '$tlp_penyelenggara', '$username_penyelenggara', '$password_penyelenggara', '$fotobaru', '$id_level', '$status_penyelenggara')") or die(mysqli_error($koneksi));
+						$insert = mysqli_query($koneksi, "INSERT INTO penyelenggara(nama_penyelenggara, nama_lomba, lokasi_lomba, waktu_awal_lomba,waktu_akhir_lomba,id_kategori, email_penyelenggara, tlp_penyelenggara, username_penyelenggara, password_penyelenggara, pembayaran_penyelenggara, id_level, status_penyelenggara,logo) VALUES('$nama_penyelenggara', '$nama_lomba', '$lokasi_lomba', '$waktu_awal_lomba', '$waktu_akhir_lomba', '$id_kategori', '$email_penyelenggara', '$tlp_penyelenggara', '$username_penyelenggara', '$password_penyelenggara', '$fotobaru', '$id_level', '$status_penyelenggara','$logo')") or die(mysqli_error($koneksi));
 							if($insert)
 							{
 								header("location: ../index.php");
